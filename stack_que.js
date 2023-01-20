b69a5ded043dfda97afced8ad3f29f5ef0810009
tonyxuan2021 <tonyyexuan@gmail.com>
2023-01-20 14:17:00 -0800

    added queue enqueue and dequeue method

--- stack_que.js
+++ stack_que.js
@@ -43,3 +43,40 @@ class Stack {
     }
   }
 }
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    let newNode = new Node(val);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+
+    return this;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+
+    let temp = this.first;
+
+    if (this.size === 1) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
