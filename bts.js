e440a6e1512edabc6a81b004e3651671aaa8ab94
tonyxuan2021 <tonyyexuan@gmail.com>
2023-01-30 18:48:24 -0800

    added post method

--- bts.js
+++ bts.js
@@ -102,6 +102,16 @@ class BinarySearchTree {
 
     return data;
   }
-  DFSPostOrder() {}
+
+  DFSPostOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+
+    traverse(this.root);
+  }
   DFSInOrder() {}
 }
