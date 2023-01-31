e420d075f913c580b9d407a849b9cbf2b0a8d207
tonyxuan2021 <tonyyexuan@gmail.com>
2023-01-30 17:17:33 -0800

    added DFS pre order

--- bts.js
+++ bts.js
@@ -89,4 +89,19 @@ class BinarySearchTree {
 
     return data;
   }
+
+  DFSPreOrder() {
+    let data = [];
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+
+    return data;
+  }
+  DFSPostOrder() {}
+  DFSInOrder() {}
 }
