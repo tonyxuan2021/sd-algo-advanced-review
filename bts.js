1240503ab1959e737161fb64e5c88d01103f65a8
tonyxuan2021 <tonyyexuan@gmail.com>
2023-01-30 20:35:53 -0800

    added tree inorder method

--- bts.js
+++ bts.js
@@ -113,5 +113,14 @@ class BinarySearchTree {
 
     traverse(this.root);
   }
-  DFSInOrder() {}
+  DFSInOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+  }
 }
